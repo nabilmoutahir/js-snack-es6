dedf55674e3186782f365df758f6463194b8f58c
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-31 21:27:09 +0100

    feat: add array of objects

---
+++ snack 3/js/main.js
@@ -0,0 +1,41 @@
+
+const bikes = [
+    {
+        'name': 'red bike',
+        'weight': 45
+    }, 
+    {
+        'name': 'blue bike',
+        'weight': 62
+    }, 
+    {
+        'name': 'green bike',
+        'weight': 123
+    }, 
+    {
+        'name': 'yellow bike',
+        'weight': 89
+    }, 
+    {
+        'name': 'black bike',
+        'weight': 90
+    }, 
+    {
+        'name': 'white bike',
+        'weight': 36
+    }, 
+    {
+        'name': 'grey bike',
+        'weight': 76
+    }, 
+    {
+        'name': 'orange bike',
+        'weight': 94
+    }, 
+    {
+        'name': 'violet bike',
+        'weight': 54
+    }, 
+]
+
+console.log(bikes)
